35264befc70848888ccd6b33c94656112d68edd6
Vasya-556 <vkobrin556@gmail.com>
2023-07-13 22:50:10 +0300

    third commit

--- users/models.py
+++ users/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+import datetime
 
 class User(models.Model):
     nickname = models.CharField('Nickname', max_length=20)
+    login = models.CharField('Login', max_length=20, default='login')
+    password = models.CharField('Password', max_length=20, default='password')
+    sex = models.CharField('Sex', max_length=10, default='none')
     date = models.DateField('Date of Registration')
     about = models.CharField('About', max_length=250)
     photo = models.ImageField('Photo', upload_to='user_photos/')
+    dob = models.DateField('Date of birth', default=datetime.date(2012, 12, 12).strftime('%Y-%m-%d'))
--- users/views.py
+++ users/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import User
 
-# Create your views here.
 def user(request):
     return render(request, 'users/user.html')
 
 def login(request):
+    if request.method == 'POST':
+        data = request.POST
+        email = data['email']
+        nickname = data['nickname']
+        password = data['password']
+        dob = data['dob']
+        sex = data['sex']
+
+        # Perform additional checks and database operations
+        # Check if the email or nickname already exists in the database
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'error': 'Email already exists.'}, status=400)
+
+        if User.objects.filter(nickname=nickname).exists():
+            return JsonResponse({'error': 'Nickname already exists.'}, status=400)
+
+        # Create a new User instance
+        user = User(email=email, nickname=nickname, password=password, dob=dob, sex=sex)
+        user.save()
+
+        # Return a success response
+        return JsonResponse({'message': 'User created successfully!'})
+
     return render(request, 'users/login.html')
